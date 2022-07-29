d51f1a8f7b2f5416107b2fb5ec0980a5e671f839
Vincent Le <vincent.tn.le@gmail.com>
2022-07-29 09:41:01 -0400

    added functionality for mouseover

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// Default configuration
 const container = document.querySelector(".grid-container")
 
-container.style.color = 'blue';
+let colour = 'black';
 
+function changeColour(g) {
+    this.style.backgroundColor = colour;
+}
 
 function createGrid(n) {
+    // Create empty grid
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= n; j++) {
             var newDiv = document.createElement("div");
@@ -11,12 +16,36 @@ function createGrid(n) {
             newDiv.style.width = '10px';
             newDiv.style.height = '10px';
             newDiv.style.margin = '1px';
-            newDiv.className = `grid ${i} ${j}`;
+            newDiv.className = `grid`;
             container.appendChild(newDiv);
         }
     }
     container.style.gridTemplateColumns = `repeat(${n}, minmax(0,1fr))`;
     container.style.gridTemplateRows = `repeat(${n}, minmax(0,1fr))`;
+
+    // Add colouring
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach((g) => {
+        g.addEventListener("mouseenter", changeColour);
+    });
 };
 
-createGrid(50);
+createGrid(16);
+
+//
+function newGrid() {
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach((g) => {
+        g.remove();
+    });
+    
+    let num = parseInt(prompt("Number of Squares per side?"));
+    //console.log(typeof(num));
+    do {
+        if (!isNaN(num)) break;
+        num = parseInt(prompt("Please enter a valid number."))
+    } while (isNaN(num))
+    console.log(num);
+    console.log(typeof(num));
+    createGrid(num);
+}
